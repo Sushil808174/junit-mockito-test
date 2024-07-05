d04a51db617a513aa0c6a0a963a496167708f3b1
Susheel Kumar <susheel.k@inspironlabs.com>
2024-07-05 10:50:09 +0530

    project added

---
+++ Unit-testing/src/main/java/com/unit_testing/Unit_testing/repository/VehicleRepository.java
@@ -0,0 +1,12 @@
+package com.unit_testing.Unit_testing.repository;
+
+import com.unit_testing.Unit_testing.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
+    public List<Vehicle> findByIsAvailableFalse();
+}
---
+++ Unit-testing/src/main/java/com/unit_testing/Unit_testing/service/UserService.java
@@ -0,0 +1,48 @@
+package com.unit_testing.Unit_testing.service;
+
+import com.unit_testing.Unit_testing.entity.User;
+import com.unit_testing.Unit_testing.repository.ServiceRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final ServiceRepo serviceRepo;
+    private final AccountService accountService;
+    public User save(User user){
+        return serviceRepo.save(user);
+    }
+    public List<User> getUsers(){
+        return serviceRepo.findAll();
+    }
+    public Optional<User> findById(Integer id){
+       return serviceRepo.findById(id);
+    }
+
+    public void deleteUser(Integer id){
+        Optional<User> byId = serviceRepo.findById(id);
+        if(byId.isPresent()){
+            serviceRepo.deleteById(id);
+        }else{
+            throw new RuntimeException("error");
+        }
+    }
+
+    public static String getAccountName(){
+        AccountService accountService1 = new AccountService();
+        return accountService1.getAccountName();
+    }
+
+    public String check(User user) {
+        if(user == null){
+            return "Error User not found";
+        }else if(user.getName().length() > 10){
+            return "Name is too long";
+        }
+        return user.getName();
+    }
+}
